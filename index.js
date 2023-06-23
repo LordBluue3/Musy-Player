8194a4f3cef544e55f0f383688f692993ceb15b9
Vinnih-1 <vinifantal@hotmail.com>
2023-06-23 08:38:44 -0300

    :white_check_mark: add notification track manager

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import {AppRegistry} from 'react-native'
+import { AppRegistry } from 'react-native'
 import App from './src/App'
-import {name as appName} from './app.json'
-import TrackPlayer from 'react-native-track-player'
-import {PlaybackService} from './src/services/PlaybackService'
+import { name as appName } from './app.json'
+import TrackPlayer, { Capability, Event } from 'react-native-track-player'
+import { PlaybackService } from './src/services/PlaybackService'
 
 AppRegistry.registerComponent(appName, () => App)
 TrackPlayer.registerPlaybackService(() => PlaybackService)
-TrackPlayer.setupPlayer()
+TrackPlayer.setupPlayer().then(() => {
+	TrackPlayer.addEventListener(Event.RemoteNext, () => TrackPlayer.skipToNext())
+	TrackPlayer.addEventListener(Event.RemotePrevious, () => TrackPlayer.skipToPrevious())
+
+	TrackPlayer.updateOptions({
+		capabilities: [
+			Capability.Play,
+			Capability.Stop,
+			Capability.Pause,
+			Capability.SkipToNext,
+			Capability.SkipToPrevious
+		],
+    
+		compactCapabilities: [ Capability.Play, Capability.Pause, Capability.SkipToNext, Capability.SkipToPrevious ]
+	})
+})
