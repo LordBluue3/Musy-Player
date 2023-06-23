ffe4cc10037c0241f06b4d3258498c86da2ca0ca
Vinnih-1 <vinifantal@hotmail.com>
2023-06-23 01:56:32 -0300

    :white_check_mark: add Player page

--- src/pages/player/index.tsx
+++ src/pages/player/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import { Text, TouchableOpacity } from 'react-native'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Image, TouchableOpacity } from 'react-native'
 import { StackNavigationProp } from '@react-navigation/stack'
+import { ArtistText, Background, MusicImage, MusicInformation, MusicPlayingView, NavToSongs, Navbar, PlayerButtons, PlayerIcons, PlayerManager, PlayerProgressBar, PlayerProgressTimer, SettingsButton, Text, Title } from './styles'
+
+import * as Progress from 'react-native-progress'
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import TrackPlayer, { Event, State, useProgress } from 'react-native-track-player'
 
 interface RootStackParamList {
     Songs: undefined;
@@ -12,14 +19,134 @@ interface PlayerScreenProps {
     navigation: StackNavigationProp<RootStackParamList, 'Songs'>
 }
 
+interface MusicProps {
+	title: string;
+	artist: string;
+	duration: string;
+	position: string;
+}
+
 export function Player({navigation}: PlayerScreenProps) {
+	const [paused, setPaused] = useState<boolean>(false)
+	const [music, setMusic] = useState<MusicProps>()
+	const progress = useProgress()
+
+	TrackPlayer.addEventListener(Event.RemotePlay, () => { TrackPlayer.play(); setPaused(false) })
+	TrackPlayer.addEventListener(Event.RemotePause, () => { TrackPlayer.pause(); setPaused(true) })
+
+	useEffect(() => {
+		setTrack()
+	}, [])
+
+	useEffect(() => {
+		console.log(progress)
+	}, [progress])
+
+	useEffect(() => {
+		TrackPlayer.addEventListener(Event.PlaybackTrackChanged, setTrack)
+	}, [])
+
+	function setTrack() {
+		TrackPlayer.getCurrentTrack().then((track) => {
+			TrackPlayer.getTrack(Number(track)).then(async (track) => {
+				const title = track?.title ?? ''
+				const artist = track?.artist ?? ''
+				const durationSeconds = await TrackPlayer.getDuration()
+				const positionSeconds = await TrackPlayer.getPosition()
+
+				const duration = `${Math.floor(durationSeconds / 60)}:${(Math.floor(durationSeconds) % 60)}`
+				const position = `${Math.floor(positionSeconds / 60)}:${(Math.floor(positionSeconds) % 60).toString().padStart(2, '0')}`
+
+				setMusic({ title: title, artist: artist, duration: duration, position: position })
+			})
+		})
+	}
+
+	async function handlePlayPause() {
+		const state = await TrackPlayer.getState()
+
+		if (state === State.Playing) {
+			TrackPlayer.pause()
+			setPaused(true)
+		} else {
+			TrackPlayer.play()
+			setPaused(false)
+		}
+	}
 	return(
-		<View style={{flex: 1, backgroundColor: 'black'}}>
-			<TouchableOpacity onPress={() => {
-				navigation.navigate('Songs')
-			}}>
-				<Text>teste</Text>
-			</TouchableOpacity>
-		</View>
+		<Background>
+			<Navbar>
+				<NavToSongs onPress={() => navigation.navigate('Songs')}>
+					<Ionicons name='chevron-down-outline' size={35}/>
+				</NavToSongs>
+				<Title>Musy Player</Title>
+				<SettingsButton>
+					<Ionicons name='ellipsis-vertical' size={25}/>
+				</SettingsButton>
+			</Navbar>
+
+			<MusicInformation>
+				<Text>Song</Text>
+				<MusicImage>
+					<Image style={{maxHeight: 250, maxWidth: 250, borderRadius: 40}} source={require('../../assets/music-icon.png')} />
+				</MusicImage>
+				<MusicPlayingView>
+					<Title>{music?.title}</Title>
+					<ArtistText>{music?.artist}</ArtistText>
+				</MusicPlayingView>
+				<PlayerManager>
+					<PlayerIcons>
+						<TouchableOpacity>
+							<Ionicons name='volume-medium' size={25} />
+						</TouchableOpacity>
+
+						<TouchableOpacity>
+							<MaterialCommunityIcons color={'#ECECEC'} name='playlist-play' size={25} />
+						</TouchableOpacity>
+
+						<TouchableOpacity>
+							<Ionicons color={'gray'} name='shuffle' size={25} />
+						</TouchableOpacity>
+
+						<TouchableOpacity>
+							<Ionicons color={'gray'} name='repeat' size={25} />
+						</TouchableOpacity>
+
+						<TouchableOpacity>
+							<Ionicons color={'#ECECEC'} name='heart-sharp' size={25} />
+						</TouchableOpacity>
+					</PlayerIcons>
+					<PlayerProgressBar>
+						<Progress.Bar 
+							borderWidth={0} 
+							color='#ECECEC' 
+							unfilledColor='#1A1A1A' 
+							height={3} width={330} 
+							progress={(Math.floor(progress.position) / Math.floor(progress.duration))} />
+						<PlayerProgressTimer>
+							<ArtistText>{music?.position}</ArtistText>
+							<ArtistText>{music?.duration}</ArtistText>
+						</PlayerProgressTimer>
+					</PlayerProgressBar>
+					<PlayerButtons>
+						<TouchableOpacity
+							onPress={() => TrackPlayer.skipToPrevious()}
+						>
+							<Ionicons color={'#ECECEC'} name='play-skip-back' size={40} />
+						</TouchableOpacity>
+
+						<TouchableOpacity onPress={handlePlayPause}>
+							<FontAwesome5 color={'#ECECEC'} name={paused ? 'play' : 'pause'} size={40} />
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							onPress={() => TrackPlayer.skipToNext()}
+						>
+							<Ionicons color={'#ECECEC'} name='play-skip-forward' size={40} />
+						</TouchableOpacity>
+					</PlayerButtons>
+				</PlayerManager>
+			</MusicInformation>
+		</Background>
 	)
 }
---
+++ src/pages/player/styles.ts
@@ -0,0 +1,107 @@
+import { styled } from 'styled-components/native'
+
+export const Background = styled.View`
+    flex: 1;
+    background-color: ${props => props.theme.background};
+`
+
+export const Navbar = styled.View`
+    flex: 1;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    max-height: 10%;
+    gap: 70px;
+`
+
+export const NavToSongs = styled.TouchableOpacity`
+`
+
+export const Title = styled.Text`
+    color: #ECECEC;
+    font-weight: 800;
+    font-size: 20px;
+`
+
+export const SettingsButton = styled.TouchableOpacity`
+    color: #ECECEC;
+    font-weight: 800;
+    font-size: 20px;
+`
+
+export const MusicInformation = styled.View`
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20%;
+`
+
+export const Text = styled.Text`
+    color: #ECECEC;
+    font-size: 16px;
+    font-weight: 900;
+`
+export const MusicImage = styled.View`
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 13%;
+`
+
+export const MusicPlayingView = styled.View`
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 15%;
+    width: 100%;
+    max-height: 15%;
+    justify-content: center;
+`
+
+export const ArtistText = styled.Text`
+    color: #FFF;
+    font-weight: 500;
+    font-size: 11px;
+`
+
+export const PlayerManager = styled.View`
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+    max-height: 30%;
+`
+
+export const PlayerIcons = styled.View`
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    max-height: 30%;
+`
+
+export const PlayerProgressBar = styled.View`
+    flex: 1;
+    width: 100%;
+    max-height: 15%;
+    justify-content: center;
+    align-items: center;
+`
+
+export const PlayerProgressTimer = styled.View`
+    flex: 1;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    max-height: 50%;
+`
+
+export const PlayerButtons = styled.View`
+    flex: 1;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin-top: 5%;
+    width: 60%;
+`
